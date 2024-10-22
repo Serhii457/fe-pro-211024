9c8a9022cd2680347013a4f1219aed07f848ef6f
Serhii457 <manushenko7@gmail.com>
2024-10-23 02:36:04 +0300

    hw01

---
+++ 01/hw01/hw01.js
@@ -0,0 +1,19 @@
+let userNickname = 'Serhii';
+let userScore = 10;
+
+let age = 41;
+console.log(age);
+age += 5;
+console.log(age);
+
+let isStudent = true;
+console.log(isStudent);
+isStudent = false;
+console.log(isStudent);
+
+let item = 10;
+console.log(item);
+item = 'ten';
+console.log(item);
+item = true;
+console.log(item);
